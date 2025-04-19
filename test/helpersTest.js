5d707d8c8be2b4b23a10c1862aa2f777c756e47e
Brendan Brown <brendan94brown@gmail.com>
2025-04-18 20:52:18 -0700

    Added second assertion to complete testing of helper function

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -20,7 +20,11 @@ describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-
     assert.equal(user.id, expectedUserID);
+  });
+
+  it("should return undefined if given an email that isn't in the database", function() {
+    const user = getUserByEmail("nonexistent@example.com", testUsers);
+    assert.isUndefined(user);
   });
 });
