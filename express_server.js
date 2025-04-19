591319f61d68e49508492d2d6dec3e1a9f8ed923
Brendan Brown <brendan94brown@gmail.com>
2025-04-18 17:49:32 -0700

    Applied bcrypt hashed passwords to test user passwords in users object, for consistency

--- express_server.js
+++ express_server.js
@@ -24,12 +24,12 @@ const users = {
   userRandomID: {
     id: "userRandomID",
     email: "user@example.com",
-    password: "purple-monkey-dinosaur",
+    password: bcrypt.hashSync("purple-monkey-dinosaur", 10),
   },
   user2RandomID: {
     id: "user2RandomID",
     email: "user2@example.com",
-    password: "dishwasher-funk",
+    password: bcrypt.hashSync("dishwasher-funk", 10),
   },
 };
 
