c5c6e386bda8ac9c8f270c7f4e275190d00f6570
Brendan Brown <brendan94brown@gmail.com>
2025-04-18 20:43:38 -0700

    Added test assertion to helpersTest file, and confirmed it passed

--- test/helpersTest.js
+++ test/helpersTest.js
@@ -16,9 +16,11 @@ const testUsers = {
 };
 
 describe('getUserByEmail', function() {
+
   it('should return a user with valid email', function() {
     const user = getUserByEmail("user@example.com", testUsers)
     const expectedUserID = "userRandomID";
-    // Write your assert statement here
+
+    assert.equal(user.id, expectedUserID);
   });
 });
